9d076945577a2fe2989d9a63e8f2489999fa8103
fjaviermp <fjaviermpinf@gmail.com>
2021-11-23 14:20:36 +0100

    Ventana he olvidado contraseña

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,10 +1,11 @@
+<link rel="shortcut icon" type="image/jpg" href="favicons/favicon.ico"/>
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="/img/escudoCelia.png" alt="logo Celia" class='logoLogin'>
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-sm text-black-600">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
         </div>
 
@@ -21,14 +22,17 @@
 
             <div class="block">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input placeholder='email@dominio.com' id="email" class="block mt-1 w-full inputLogin" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
+            <div class="flex items-center justify-end mt-4 centrarBotones">
+                <x-jet-button class="botonIniciar">
                     {{ __('Email Password Reset Link') }}
                 </x-jet-button>
             </div>
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
+<div class="imagenFondo"></div>
+
+<link rel="stylesheet" href="estilos/login/loginYregistro.css">
