be3b02ad30aade042ce400330a55143a7e92b6e7
FMRedondo <franciscomanuelredondojimenez@gmail.com>
2021-12-10 12:18:06 +0100

    cambios en vista comentarios

--- public/js/admin/comments.js
+++ public/js/admin/comments.js
@@ -17,6 +17,7 @@ function index() {
                                     <tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
                                         <th class="px-4 py-3">Comentarios</th>
                                         <th class="px-4 py-3">Puntos</th>
+                                        <th class="px-4 py-3 text-center">Acciones</th>
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white contenido">
@@ -31,14 +32,17 @@ function index() {
 
 
 
-            response.forEach(function (datos) {
+            response.forEach(function (data) {
                 let contenidoTabla = `
                 <tr class="text-gray-700">
                     <td class="px-4 py-3 text-xs border">
-                        <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> ${datos.content} </span>
+                        <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> ${data.content} </span>
                     </td>
                     <td class="px-4 py-3 text-xs border">
-                    <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> ${datos.idPunto} </span>
+                    <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> ${data.idPunto} </span>
+                    </td>
+                    <td class="px-4 py-3 text-sm border d-flex flex-row justify-content-around">
+                        <button type="button" class="btn btn-danger btnDelType" data-id='${data.id}'>Eliminar</button>
                     </td>
                 </tr>`;
 
@@ -77,7 +81,8 @@ function searchComments(){
                         <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> ${datos.content} </span>
                     </td>
                     <td class="px-4 py-3 text-xs border">
-                    <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> ${datos.idPunto} </span>
+                    <span class="tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama tumama "></span><span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> 
+                    dolar{datos.idPunto} </span>
                     </td>
                 </tr>`;
             });
@@ -86,11 +91,42 @@ function searchComments(){
         },
 
         error: function () {
-            alert("error en la peticion");
+            alert("error en le peticion");
         }
     });
 }
 
+$(".btnDelComments").click(showDelComments);
+
+function showDelComments() {
+    $(".delCommentsPanel").show();
+    let id = $(this).data("id");
+
+    // Funcion para borrar categorias
+    $(".btnWindow").click(function() {
+        if ($(this).data("val")) {
+            var params = {
+                "id": id,
+                "_token": $('meta[name="csrf-token"]').attr('content'),
+            }
+            $.ajax({
+                data: params,
+                url: '/admin/comentarios/deleteComments',
+                type: 'post',
+                success: function (response) {
+                    let route = "table tbody #" + id;
+                    $(route).remove();
+                    $(".delTPanel").hide();
+                },
+                error: function (response) {
+                    alert("Error en la peticion");            
+                },
+            });
+        }
+        if (!($(this).data("val")))
+            $(".delCommentsPanel").hide();
+    })
+}
 
 
 
