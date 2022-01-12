54e6a1bcc135d250f3ef5203973909f9058a43ce
FMRedondo <franciscomanuelredondojimenez@gmail.com>
2022-01-12 18:06:51 +0100

    pasar los puntos de interes a js vanilla :)

--- public/js/admin/interestPoints.js
+++ public/js/admin/interestPoints.js
@@ -1,7 +1,82 @@
-$(document).ready(() => {
-  
-    var parametros = [];
-    const token = $('meta[name="csrf-token"]').attr('content');
+const _token = $('meta[name="csrf-token"]').attr('content');
+
+/**
+ * 
+ * @param {string} url URL donde queremos realizar la peticion GET
+ * @returns devuelve un JSON con los datos pedidos al servidor
+ */
+const getData = async (url = '') => {
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+    });
+
+    return response.json();
+}
+
+
+const index = async () => {
+    var response;
+    await getData('/admin/puntosInteres/getPoints', {}).then((res) => {
+        response = res;
+    })
+    const contenidoVista = document.querySelector(".contenidoPuntos")
+    contenidoVista.innerHTML = "";
+    response.forEach(data => {
+        let contenidoTabla = `
+            <div class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden w-32" style="width: 30%">
+            <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-4"
+                src="https://picsum.photos/720/400" alt="Image Size 720x400" />
+            <div class="p-4">
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2 whitespace-nowrap truncate">
+                    ${data.name}
+                </h2>
+                <p class="text-gray-600 font-light text-md mb-3">
+                ${data.description}
+                </p>
+                <div class="py-4 border-t border-b text-xs text-gray-700">
+                    <div class="grid grid-cols-6 gap-1">
+                        <div class="col-span-2">
+                            Beds:
+                            <span
+                                class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-red-400 rounded-full">3</span>
+                        </div>
+                        <div class="col-span-2">
+                            Bathrooms:
+                            <span
+                                class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-green-400 rounded-full">2</span>
+                        </div>
+                        <div class="col-span-2">
+                            Area:
+                            <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-blue-400 rounded-full">110m <sup>2</sup></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex items-center mt-2 flex-row-reverse" data-id=${data.id}>
+                    <a class="btn btn-success rounded-circle flex editButton" data-id=${data.id}><i class="fa-solid fa-pen-to-square"></i></a>
+                </div>
+            </div>    
+        </div>`;
+
+        contenidoVista.innerHTML += contenidoTabla;
+    });
+}
+
+index();
+
+
+
+
+
+/*
 
     $.ajax({
         data: parametros,
@@ -40,11 +115,11 @@ $(document).ready(() => {
                             </div>
                         </div>
                     </div>
-        
+
                     <div class="flex items-center mt-2 flex-row-reverse" data-id=${data.id}>
-                        <a class="btn btn-success rounded-circle flex editButton" data-id=${data.id}><i class="fa-solid fa-pen-to-square" data-id=${data.id}></i></a>
+                        <a class="btn btn-success rounded-circle flex editButton" data-id=${data.id}><i class="fa-solid fa-pen-to-square"></i></a>
                     </div>
-                </div>    
+                </div>
             </div>`;
 
                 $(".contenidoPuntos").append(contenidoTabla);
@@ -70,7 +145,7 @@ const showEditPanel = () => {
     console.log(Object.values(this))
 
     var parametros =  {
-        "id": id, 
+        "id": id,
         "_token": token,
     }
 
@@ -82,7 +157,7 @@ const showEditPanel = () => {
         type: 'POST',
 
         success: function (response) {
-          
+
            /*response.forEach(function (data) {
                 let contenidoTabla = ``;
 
@@ -90,7 +165,7 @@ const showEditPanel = () => {
 
             });
 
-            
+
 
             alert(response)
 
@@ -100,12 +175,12 @@ const showEditPanel = () => {
             alert("error en la peticion");
         }
 
-        
+
     });
 
 
     /*
-    
+
      <div class="rounded-xl shadow-lg bg-white modifyPanel bigPanel">
                         <div class="d-flex align-items-center px-4" style="height:10%; background-color:#F4F4F4;">
                             <p class="display-4 p-1 w-75">Editar un recurso</p>
@@ -140,7 +215,7 @@ const showEditPanel = () => {
                                     <input type="text" value="${data.autor}" data-field='autor' class="autor form-control ResourceField" id="resourceAutor" style="border-radius: 0.25rem">
                                     <div class="form-text">El autor es la persona que ha fotografiado/grabado/narrado el recurso.</div>
                                 </div>
-                            
+
                                 <div class="mb-4">
                                     <label for="resourceUser" class="form-label">Subido por:</label>
                                     <p class="form-control" id="resourceUser" style="border-radius: 0.25rem">${data.user}</p>
@@ -150,7 +225,7 @@ const showEditPanel = () => {
                                     <label for="resourceType" class="form-label">Tipo de recurso:</label>
                                     <p class="form-control" id="resourceType" style="border-radius: 0.25rem">${data.type}</p>
                                 </div>
-                        
+
                                 <div class="mt-5">
                                     <div class="mb-2 d-flex">
                                         <p class="w-25 font-bold d-flex align-items-center justify-content-end mr-2">Fecha de creación:</p>
@@ -166,7 +241,7 @@ const showEditPanel = () => {
                     <div>
                 `;
     */
-}
+
 
 
 // logica de la parte modal de los untos de interes
