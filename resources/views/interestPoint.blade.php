212140b2d5c1aae33af11c28e5ad7dc87f0c7f33
fjaviermp <fjaviermpinf@gmail.com>
2022-01-17 10:03:09 +0100

    Redondo me esta jodiendo la vida

--- resources/views/interestPoint.blade.php
+++ resources/views/interestPoint.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/estilosGenerales.css">
-    <link rel="stylesheet" href="/estilos/puntosDeInteres.css">
+    <link rel="stylesheet" href="/estilos/puntoDeInteres.css">
     <title>Si y solo si</title>
 </head>
 <body>
@@ -20,9 +20,35 @@
         </div>
         
         <div id="information">
-
-        <div id="pointThumbnail">
             <div class="pointContent">
+                <div class="contentHeader">
+                    <div class="contentTitle">
+                        <p>Categoría: Tanques chulos</p>
+                        <h1>Panzerkampwagen VI Tiger I</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod maiores eum accusantium libero optio quos laborum deleniti necessitatibus vitae.</p>
+                    </div>
+                    <div class="contentThumbnail">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_101I-299-1805-16%2C_Nordfrankreich%2C_Panzer_VI_%28Tiger_I%29_cropped.jpg" alt="Miniatura del punto">
+                    </div>
+                </div>
+                <div class="contentText">
+                    <div class="entryText">
+                        <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa ab natus placeat ad dolores corrupti alias atque ut nesciunt quibusdam quas reiciendis doloremque fuga sed explicabo distinctio, consequuntur saepe vero.
+                    Repudiandae inventore exercitationem tenetur, similique adipisci laborum veniam sed suscipit labore quia dolor et eligendi nam distinctio. Asperiores error nisi reprehenderit quasi ad adipisci exercitationem natus deleniti, voluptas, tempore architecto.
+                    Impedit ea totam praesentium dolor aliquid cum id quaerat quae, error harum, repudiandae dolorum nulla similique reprehenderit assumenda. Error, est ad vel placeat reprehenderit minima. Nostrum dolores itaque repellendus adipisci.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="multimedia">
+            <div class="videosPanel">
+
+            </div>
+            <div class="audioPanel">
+
             </div>
         </div>
 
