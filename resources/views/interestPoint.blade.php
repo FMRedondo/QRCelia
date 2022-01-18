3bdf67440c4c6ea5815ad67ffdc7693effee2d37
Francisco Javier Martínez Plaza <74726752+fjaviermp@users.noreply.github.com>
2022-01-18 11:59:18 +0100

    Carta de audios, separador

--- resources/views/interestPoint.blade.php
+++ resources/views/interestPoint.blade.php
@@ -64,26 +64,40 @@
         </div>
 
         <div id="multimedia">
-
             <div class="videosPanel">
+              <div class="wrapper">
+                <separador-component texto='multimedia' color='white'></separador-component>
+              </div>
                 <div class="swiper swiper-videos videos">
                     <div class="swiper-wrapper">
                       <div class="swiper-slide">
-                        <video controls src="/resources/video/videoprueba.mp4"></video>
+                        <video controls>
+                          <source src="/resources/video/videoprueba.mp4" type="video/mp4">
+                          <span class="error">Tu navegador no es compatible con este recurso</span>
+                        </video>
                       </div>
                       <div class="swiper-slide">
-                        <video controls src="/resources/video/videoprueba.mp4"></video>
-                      </div>
+                        <video controls>
+                          <source src="/resources/video/videoprueba.mp4" type="video/mp4">
+                          <span class="error">Tu navegador no es compatible con este recurso</span>
+                        </video>                      </div>
                       <div class="swiper-slide">
-                        <video controls src="/resources/video/videoprueba.mp4"></video>
-                      </div>
+                        <video controls>
+                          <source src="/resources/video/videoprueba.mp4" type="video/mp4">
+                          <span class="error">Tu navegador no es compatible con este recurso</span>
+                        </video>                      </div>
                     </div>
                     <div class="swiper-pagination"></div>
                 </div>
             </div>
 
             <div class="audioPanel">
-                 
+                 <div class="audio-card">
+                    <img src="/img/audio.png" alt="">
+                    <button id="1" class="buttonPlay">
+                      <i class="fa-solid fa-play"></i>
+                    </button>
+                  </div>
             </div>
 
         </div>
